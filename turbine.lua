4e639aed93a9d63d31f9e18cb45e8500814ca1f5
rkNolem <ridleykiller1994@gmail.com>
2015-01-26 19:05:29 -0500

    Update turbine.lua
    added license on startup

--- turbine.lua
+++ turbine.lua
@@ -16,6 +16,13 @@ end
 local intake = 0
 local lastSpeed = 0
 
+print([[rkTurbine  Copyright (C) 2015  Sean Harris
+This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
+This is free software, and you are welcome to redistribute it
+under certain conditions.]])
+sleep (10)
+term.clear()
+
 while true do
   
   turbine.setActive(true)
@@ -28,3 +35,18 @@ while true do
   
   sleep(5)
 end
+
+--[[
+Big-Reactors turbine management code for ComputerCraft in Lua
+Copyright (C) 2015  Sean Harris
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+You can contact the author at: ridleykiller1994@gmail.com]]
